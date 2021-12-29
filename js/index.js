8b6b04275605bd24164aca6cb15eaf1ed5392b02
Victoria <victorianastasi@gmail.com>
2021-12-29 17:15:26 -0300

    Added greeting, favicon, and changed btn styles

--- js/index.js
+++ js/index.js
@@ -22,8 +22,16 @@ window.addEventListener('load', function() {
     let coffeeList = [
         {nombre: 'Café Espresso', imagen: '../img/espresso.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 180},
         {nombre: 'Café Americano', imagen: '../img/americano.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 200},
+        {nombre: 'Cortado', imagen: '../img/cortado.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 200},
+        {nombre: 'Café con leche', imagen: '../img/cafeconleche.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
         {nombre: 'Café Latte', imagen: '../img/latte.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
-        {nombre: 'Café Mocha', imagen: '../img/mocha.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220}
+        {nombre: 'Café Mocha', imagen: '../img/mocha.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 250},
+        {nombre: 'Macciato', imagen: '../img/macciato.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
+        {nombre: 'Caramel Macciato', imagen: '../img/caramel.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
+        {nombre: 'Capuccino', imagen: '../img/capuccino.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
+        {nombre: 'Flat White', imagen: '../img/flat.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 220},
+        {nombre: 'Café Irlandes', imagen: '../img/irlandes.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 260},
+        {nombre: 'Café Amaretto', imagen: '../img/amaretto.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 260}        
     ];
     let juiceList = [
         {nombre: 'Limonada', imagen: '../img/limon.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 150},
@@ -33,23 +41,37 @@ window.addEventListener('load', function() {
         {nombre: 'Jugo de Durazno', imagen: '../img/durazno.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 150}
     ];
     let sweetList = [
-        {nombre: 'Cheesecake', imagen: '../img/cheesecake.png', tacc: '', vegan: '', price: 250},
-        {nombre: 'Chocotorta', imagen: '../img/chocotorta.png', tacc: '', vegan: '', price: 230},
+        {nombre: 'Cheesecake', imagen: '../img/cheesecake.png', tacc: '', vegan: '', price: 280},
+        {nombre: 'Cheesecake de arándanos', imagen: '../img/cheesecakeblue.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 280},
+        {nombre: 'Chocotorta', imagen: '../img/chocotorta.png', tacc: '', vegan: '', price: 250},
+        {nombre: 'Apple Pie', imagen: '../img/apple.png', tacc: '', vegan: '', price: 230},
         {nombre: 'Cupcakes', imagen: '../img/cupcakes.png', tacc: '', vegan: '', price: 180},
         {nombre: 'Donuts', imagen: '../img/donuts.png', tacc: '', vegan: '', price: 180},
+        {nombre: 'Cookies', imagen: '../img/cookies.png', tacc: '', vegan: '', price: 220},
         {nombre: 'Hotcakes', imagen: '../img/hotcakes.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 210},
         {nombre: 'Medialunas', imagen: '../img/medialunas.png', tacc: '', vegan: '', price: 150},
         {nombre: 'Tostadas con mermelada y queso blanco', imagen: '../img/toast.png', tacc: '', vegan: '', price: 180},
-        {nombre: 'Ensalada de frutas', imagen: '../img/frutas.jpg', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 200}
+        {nombre: 'Ensalada de frutas', imagen: '../img/frutas.jpg', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 200}        
     ];
     let saltyList = [
         {nombre: 'Tostado de jamón y queso', imagen: '../img/sandwich.png', tacc: '', vegan: '', price: 250},
         {nombre: 'Panini de pollo y vegetales', imagen: '../img/sandwich2.png', tacc: '', vegan: '', price: 270},
         {nombre: 'Panini vegetariano', imagen: '../img/sandwich3.png', tacc: '', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 250},
         {nombre: 'Tostada con guacamole', imagen: '../img/tavocado.jpg', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 230},
-        {nombre: 'Huevos revueltos', imagen: '../img/huevos.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 200},
+        {nombre: 'Huevos revueltos', imagen: '../img/huevos.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '', price: 200},
         {nombre: 'Ensalada', imagen: '../img/salad.png', tacc: '<img class="img-icon" src="./img/iconTacc.png" alt="Sin T.A.C.C.">', vegan: '<i class="fas fa-seedling icon-vegan"></i>', price: 230}
     ];
+
+    const greeting = () => {
+        this.document.getElementById('greeting').innerHTML =
+            `
+                <h2>Bienvenidos a Coffee Shop!!</h2>
+                <p>Conocé nuestro menú</p>
+                <p><i class="fas fa-angle-double-down animate__animated animate__fadeOutDown animate__infinite infinite animate__slow"></i></p>
+            `;
+    }
+
+    greeting();
 
     function showLists (list, id){
         let acu =  ``;
@@ -84,37 +106,56 @@ window.addEventListener('load', function() {
     let btnSalty = document.getElementById("salty-btn");
 
     btnCoffee.addEventListener('click', () =>{
-        document.getElementById("coffee-menu").classList.remove("hide");
+        document.getElementById('greeting').classList.add("hide");
         document.getElementById("coffee-menu").classList.add("show");
         document.getElementById("juice-menu").classList.remove("show");
         document.getElementById("sweet-menu").classList.remove("show");
         document.getElementById("salty-menu").classList.remove("show");
         document.getElementById("header-icon").style.backgroundImage = "url('../img/bg-coffee.png')";
-        document.getElementById("header-text").style.color = "rgb(241,237,206)";
+        document.getElementById("header-text").style.color = "rgb(241, 237, 206)";
+        btnCoffee.classList.add("btn-coffee-active");
+        btnJuice.classList.remove("btn-juice-active");
+        btnSweet.classList.remove("btn-sweet-active");
+        btnSalty.classList.remove("btn-salty-active");
     });
     btnJuice.addEventListener('click', () =>{
-        document.getElementById("coffee-menu").classList.add("hide");
+        document.getElementById('greeting').classList.add("hide");
+        document.getElementById("coffee-menu").classList.remove("show");
         document.getElementById("juice-menu").classList.add("show");
         document.getElementById("sweet-menu").classList.remove("show");
         document.getElementById("salty-menu").classList.remove("show");
         document.getElementById("header-icon").style.backgroundImage = "url('../img/bg-juice.png')";
-        document.getElementById("header-text").style.color = "rgb(245,245,196)";
+        document.getElementById("header-text").style.color = "rgb(245, 245, 196)";
+        btnCoffee.classList.remove("btn-coffee-active");
+        btnJuice.classList.add("btn-juice-active");
+        btnSweet.classList.remove("btn-sweet-active");
+        btnSalty.classList.remove("btn-salty-active");
     });
     btnSweet.addEventListener('click', () =>{
-        document.getElementById("coffee-menu").classList.add("hide");
+        document.getElementById('greeting').classList.add("hide");
+        document.getElementById("coffee-menu").classList.remove("show");
         document.getElementById("juice-menu").classList.remove("show");
         document.getElementById("sweet-menu").classList.add("show");
         document.getElementById("salty-menu").classList.remove("show");
         document.getElementById("header-icon").style.backgroundImage = "url('../img/bg-sweet.png')";
-        document.getElementById("header-text").style.color = "rgb(239,190,192)";
+        document.getElementById("header-text").style.color = "rgb(239, 190, 192)";
+        btnCoffee.classList.remove("btn-coffee-active");
+        btnJuice.classList.remove("btn-juice-active");
+        btnSweet.classList.add("btn-sweet-active");
+        btnSalty.classList.remove("btn-salty-active");
     });
     btnSalty.addEventListener('click', () =>{
-        document.getElementById("coffee-menu").classList.add("hide");
+        document.getElementById('greeting').classList.add("hide");
+        document.getElementById("coffee-menu").classList.remove("show");
         document.getElementById("juice-menu").classList.remove("show");
         document.getElementById("sweet-menu").classList.remove("show");
         document.getElementById("salty-menu").classList.add("show");
         document.getElementById("header-icon").style.backgroundImage = "url('../img/bg-salty.png')";
-        document.getElementById("header-text").style.color = "rgb(193,223,192)";
+        document.getElementById("header-text").style.color = "rgb(193, 223, 192)";
+        btnCoffee.classList.remove("btn-coffee-active");
+        btnJuice.classList.remove("btn-juice-active");
+        btnSweet.classList.remove("btn-sweet-active");
+        btnSalty.classList.add("btn-salty-active");
     });
 
     
